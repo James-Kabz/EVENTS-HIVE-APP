ee5239c2697ec770c541302f008c413bd9087b20
James-Kabz <kabogp@gmail.com>
2025-04-02 15:04:04 +0300

    feat(analytics and speed insights)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from "@/components/auth/auth-provider"
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css"
 import { Toaster } from "@/components/ui/sonner"
 
@@ -25,6 +25,7 @@ export default function RootLayout({
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             {children}
             <Toaster position="top-right" />
+            <SpeedInsights />
           </ThemeProvider>
         </AuthProvider>
       </body>
